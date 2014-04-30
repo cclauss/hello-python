cf3bb4a496ac4eb4e43ea1818332494a904ae0b6
cclauss <cclauss@bluewin.ch>
2014-04-30 12:11:22 +0200

    Show versions of Python and Flask
    Show versions of Python and Flask.

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-import os, sys
-from flask import Flask
+import flask, os, sys
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello World!<p><p><sub>Running on Python {}</sub>'.format(sys.version)
+    fmt = '<h1>Hello World!</h1><p><p><sub>Running on Python {} and Flask {}.</sub>'
+    return fmt.format(sys.version, flask.__version__)
 
 if __name__ == "__main__":
     if sys.platform == 'darwin':
