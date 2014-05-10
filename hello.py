5022d92b4ff4633d445ff558ea5443568d39f9c2
cclauss <cclauss@bluewin.ch>
2014-05-10 20:20:38 +0200

    Support both Flask and bottle
    Put Flask __or__ bottle in the `requirements.txt` file.  If both are present then Flask is used.
    
    To force the removal of old packages, change Python versions in `runtime.txt`.

--- hello.py
+++ hello.py
@@ -1,16 +1,35 @@
-import flask, os, sys, webbrowser
+#!/usr/bin/env python
 
-app = flask.Flask(__name__)
+import os, platform, sys, webbrowser
+try:
+    import flask
+except ImportError:
+    import bottle
+
+def product_url(in_name, in_domain_name, in_version):
+    fmt = '<a href="http://{}">{} {}</a>'
+    return fmt.format(in_domain_name, in_name, in_version)
+
+python_link = product_url('Python', 'python.org', platform.python_version())
+
+app = None
+web_framework_link = ''
+try:
+    app = flask.Flask(__name__)
+    web_framework_link = product_url('Flask', 'flask.pocoo.org', flask.__version__)
+except NameError:
+    app = bottle.Bottle(catchall=False)
+    web_framework_link = product_url('Bottle', 'bottlepy.org', bottle.__version__)
 
 @app.route('/')
 def hello():
-    fmt = '<h1>Hello World!</h1><p><p><sub>Running on Python {} and Flask {}.</sub>'
-    return fmt.format(sys.version, flask.__version__)
+    fmt = '<h1>Hello World!</h1><p><p><sub>Running on {} and {}.</sub>'
+    return fmt.format(python_link, web_framework_link)
 
 if __name__ == '__main__':
     if sys.platform == 'darwin':
         webbrowser.open('http://127.0.0.1:5000')
-        app.run(debug=True)  # if on Mac OSX
+        app.run(debug=True, port=5000)  # if on Mac OSX
     else:
         port = os.getenv('VCAP_APP_PORT', '5000')
         app.run(host='0.0.0.0', port=int(port))
