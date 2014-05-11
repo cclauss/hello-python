224e735d606b7f685e007fbbe0eeb59cf0daa0c6
cclauss <cclauss@bluewin.ch>
2014-05-11 12:58:03 +0200

    Update hello.py to support Flask and bottle
    Try to use Flask but fall back to bottle if Flask is not found.
    
    HTML footer shows Python and Flask/bottle versions.

--- hello.py
+++ hello.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
 import os, platform, sys, webbrowser
+
+use_flask = False
 try:
     import flask
+    use_flask = True
 except ImportError:
     import bottle
 
-def product_url(in_name, in_domain_name, in_version):
-    fmt = '<a href="http://{}">{} {}</a>'
-    return fmt.format(in_domain_name, in_name, in_version)
+def get_footer():
+    def product_url(in_name, in_domain_name, in_version):
+        fmt = '<a href="http://{}">{} {}</a>'
+        return fmt.format(in_domain_name, in_name, in_version)
 
-python_link = product_url('Python', 'python.org', platform.python_version())
+    python_link = product_url('Python', 'python.org', platform.python_version())
+    if use_flask:
+        web_framework_link = product_url('Flask', 'flask.pocoo.org', flask.__version__)
+    else:
+        web_framework_link = product_url('Bottle', 'bottlepy.org', bottle.__version__)
+    return '<sub>Running on {} and {}.</sub>'.format(python_link, web_framework_link)
 
-app = None
-web_framework_link = ''
-try:
-    app = flask.Flask(__name__)
-    web_framework_link = product_url('Flask', 'flask.pocoo.org', flask.__version__)
-except NameError:
-    app = bottle.Bottle(__name__)
-    web_framework_link = product_url('Bottle', 'bottlepy.org', bottle.__version__)
+footer = get_footer()
+app = flask.Flask(__name__) if use_flask else bottle.Bottle(__name__)
 
 @app.route('/')
 def hello():
-    fmt = '<h1>Hello World!</h1><p><p><sub>Running on {} and {}.</sub>'
-    return fmt.format(python_link, web_framework_link)
+    return '<h1>Hello World!</h1><p><p>' + footer
 
 if __name__ == '__main__':
     if sys.platform == 'darwin':
